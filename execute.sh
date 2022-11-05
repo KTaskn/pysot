b0213a5c296dd6c3263b84aca8a8bd58340442b6
KTaskn <korai.kinjo@gmail.com>
2022-11-05 22:42:10 +0900

    readmeと実行ファイルを修正

--- execute.sh
+++ execute.sh
@@ -1,14 +1,15 @@
 export PYTHONPATH=/path/to/pysot:$PYTHONPATH
-# python tools/demo_without_window.py \
-#     --config experiments/siamrpn_r50_l234_dwxcorr/config.yaml \
-#     --snapshot experiments/siamrpn_r50_l234_dwxcorr/siamrpn_r50_l234_dwxcorr.pth \
-#     --input_video_name /video/01_017 \
-#     --output_video_name ./01_017_tracking.mp4 \
-#     --init_rect 60 35 90 130
+
+INPUT=/video/fish.mp4
+OUTPUT=/video/fish_tracking.mp4
+X=500
+Y=320
+W=90
+H=50
 
 python tools/demo_without_window.py \
     --config experiments/siamrpn_r50_l234_dwxcorr/config.yaml \
     --snapshot experiments/siamrpn_r50_l234_dwxcorr/siamrpn_r50_l234_dwxcorr.pth \
-    --input_video_name /video/fish \
-    --output_video_name ./fish_tracking.mp4 \
-    --init_rect 500 320 90 50
+    --input_video_name $INPUT \
+    --output_video_name $OUTPUT \
+    --init_rect $X $Y $W $H
